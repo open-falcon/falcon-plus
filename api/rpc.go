2d07603c02ec49f31a66650c4de54683f49a5fea
laiwei <laiwei.ustc@gmail.com>
2015-05-15 10:52:16 +0800

    close established connect after recv SIGQUIT

--- api/rpc.go
+++ api/rpc.go
@@ -1,19 +1,39 @@
 package api
 
 import (
+	"container/list"
 	"log"
 	"net"
 	"net/rpc"
+	"sync"
 	"time"
 
 	"github.com/open-falcon/graph/g"
 )
 
+type conn_list struct {
+	sync.RWMutex
+	list *list.List
+}
+
+func (l *conn_list) insert(c net.Conn) *list.Element {
+	l.Lock()
+	defer l.Unlock()
+	return l.list.PushBack(c)
+}
+func (l *conn_list) remove(e *list.Element) net.Conn {
+	l.Lock()
+	defer l.Unlock()
+	return l.list.Remove(e).(net.Conn)
+}
+
 var Close_chan, Close_done_chan chan int
+var connects conn_list
 
 func init() {
 	Close_chan = make(chan int, 1)
 	Close_done_chan = make(chan int, 1)
+	connects = conn_list{list: list.New()}
 }
 
 func Start() {
@@ -52,7 +72,11 @@ func Start() {
 				continue
 			}
 			tempDelay = 0
-			go rpc.ServeConn(conn)
+			go func() {
+				e := connects.insert(conn)
+				defer connects.remove(e)
+				rpc.ServeConn(conn)
+			}()
 		}
 	}()
 
@@ -61,6 +85,13 @@ func Start() {
 		log.Println("api recv sigout and exit...")
 		listener.Close()
 		Close_done_chan <- 1
+
+		connects.Lock()
+		for e := connects.list.Front(); e != nil; e = e.Next() {
+			e.Value.(net.Conn).Close()
+		}
+		connects.Unlock()
+
 		return
 	}
 
