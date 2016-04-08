d31bcf63e723b4113c14fd8d3ea88abf76439981
laiwei <laiwei.ustc@gmail.com>
2016-04-08 17:36:43 +0800

    fix crash while sending to multi-transfer

--- g/transfer.go
+++ g/transfer.go
@@ -10,27 +10,30 @@ import (
 )
 
 var (
-	TransferLock    *sync.RWMutex                   = new(sync.RWMutex)
-	TransferClients map[string]*SingleConnRpcClient = map[string]*SingleConnRpcClient{}
+	TransferClientsLock *sync.RWMutex                   = new(sync.RWMutex)
+	TransferClients     map[string]*SingleConnRpcClient = map[string]*SingleConnRpcClient{}
 )
 
 func SendMetrics(metrics []*model.MetricValue, resp *model.TransferResponse) {
 	rand.Seed(time.Now().UnixNano())
 	for _, i := range rand.Perm(len(Config().Transfer.Addrs)) {
+		TransferClientsLock.Lock()
+
 		addr := Config().Transfer.Addrs[i]
 		if _, ok := TransferClients[addr]; !ok {
 			initTransferClient(addr)
 		}
 		if updateMetrics(addr, metrics, resp) {
+			TransferClientsLock.Unlock()
 			break
 		}
 		closeTransferClient(addr)
+
+		TransferClientsLock.Unlock()
 	}
 }
 
 func initTransferClient(addr string) {
-	TransferLock.Lock()
-	defer TransferLock.Unlock()
 	TransferClients[addr] = &SingleConnRpcClient{
 		RpcServer: addr,
 		Timeout:   time.Duration(Config().Transfer.Timeout) * time.Millisecond,
@@ -38,15 +41,11 @@ func initTransferClient(addr string) {
 }
 
 func closeTransferClient(addr string) {
-	TransferLock.Lock()
-	defer TransferLock.Unlock()
 	TransferClients[addr].close()
 	delete(TransferClients, addr)
 }
 
 func updateMetrics(addr string, metrics []*model.MetricValue, resp *model.TransferResponse) bool {
-	TransferLock.RLock()
-	defer TransferLock.RUnlock()
 	err := TransferClients[addr].Call("Transfer.Update", metrics, resp)
 	if err != nil {
 		log.Println("call Transfer.Update fail", addr, err)
