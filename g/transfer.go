11dce4eea3fb79641f383ca0b36ec2dd273dcb7e
Sosara <Sosara@Sosaras-Air.lan>
2016-04-09 18:06:49 +0800

    fix crash while sending date to multi-transfer

--- g/transfer.go
+++ g/transfer.go
@@ -17,35 +17,28 @@ var (
 func SendMetrics(metrics []*model.MetricValue, resp *model.TransferResponse) {
 	rand.Seed(time.Now().UnixNano())
 	for _, i := range rand.Perm(len(Config().Transfer.Addrs)) {
-		TransferClientsLock.Lock()
-
 		addr := Config().Transfer.Addrs[i]
 		if _, ok := TransferClients[addr]; !ok {
 			initTransferClient(addr)
 		}
 		if updateMetrics(addr, metrics, resp) {
-			TransferClientsLock.Unlock()
 			break
 		}
-		closeTransferClient(addr)
-
-		TransferClientsLock.Unlock()
 	}
 }
 
 func initTransferClient(addr string) {
+	TransferClientsLock.Lock()
+	defer TransferClientsLock.Unlock()
 	TransferClients[addr] = &SingleConnRpcClient{
 		RpcServer: addr,
 		Timeout:   time.Duration(Config().Transfer.Timeout) * time.Millisecond,
 	}
 }
 
-func closeTransferClient(addr string) {
-	TransferClients[addr].close()
-	delete(TransferClients, addr)
-}
-
 func updateMetrics(addr string, metrics []*model.MetricValue, resp *model.TransferResponse) bool {
+	TransferClientsLock.RLock()
+	defer TransferClientsLock.RUnlock()
 	err := TransferClients[addr].Call("Transfer.Update", metrics, resp)
 	if err != nil {
 		log.Println("call Transfer.Update fail", addr, err)
