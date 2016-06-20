b9a9bdfbc248aa5142282b79909603991fe88f56
Liu Yanglong <liuyanglong@lianjia.com>
2016-06-20 19:43:08 +0800

    增加更多的proc api

--- http/proc.go
+++ http/proc.go
@@ -2,12 +2,42 @@ package http
 
 import (
 	"github.com/open-falcon/hbs/cache"
+    "github.com/open-falcon/common/model"
 	"net/http"
+    "fmt"
 )
 
 func configProcRoutes() {
 	http.HandleFunc("/expressions", func(w http.ResponseWriter, r *http.Request) {
 		RenderDataJson(w, cache.ExpressionCache.Get())
+	})
+
+	http.HandleFunc("/agents", func(w http.ResponseWriter, r *http.Request) {
+		RenderDataJson(w, cache.Agents.Keys())
+	})
+
+	http.HandleFunc("/hosts", func(w http.ResponseWriter, r *http.Request) {
+        data := make(map[string]*model.Host, len(cache.MonitoredHosts.Get()))
+        for k,v := range cache.MonitoredHosts.Get() {
+            data[fmt.Sprint(k)] = v 
+        }
+		RenderDataJson(w, data)
+	})
+
+	http.HandleFunc("/strategies", func(w http.ResponseWriter, r *http.Request) {
+        data := make(map[string]*model.Strategy, len(cache.Strategies.GetMap()))
+        for k,v := range cache.Strategies.GetMap() {
+            data[fmt.Sprint(k)] = v 
+        }
+		RenderDataJson(w, data)
+	})
+
+	http.HandleFunc("/templates", func(w http.ResponseWriter, r *http.Request) {
+        data := make(map[string]*model.Template, len(cache.TemplateCache.GetMap()))
+        for k,v := range cache.TemplateCache.GetMap() {
+            data[fmt.Sprint(k)] = v 
+        }
+		RenderDataJson(w, data)
 	})
 
 	http.HandleFunc("/plugins/", func(w http.ResponseWriter, r *http.Request) {
