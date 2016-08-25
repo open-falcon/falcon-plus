4baa5c013d9dea59f26a96cad1675cbff2802509
chyeh <chyeh@cepave.com>
2016-08-25 14:32:16 +0800

    Redesign of `check` command
    Due to the redesign, the structure of the code is simpler now.

--- cmd/check.go
+++ cmd/check.go
@@ -9,10 +9,11 @@ import (
 )
 
 var Check = &cobra.Command{
-	Use:   "status [Module ...]",
+	Use:   "check [Module ...]",
 	Short: "Check the status of Open-Falcon modules",
 	Long: `
-Check if the specified Open-Falcon modules are running, not running or noexistent.
+Check if the specified Open-Falcon modules are running.
+
 Modules:
   ` + "all " + strings.Join(g.AllModulesInOrder, " "),
 	RunE: check,
@@ -22,20 +23,16 @@ func check(c *cobra.Command, args []string) error {
 	if len(args) == 0 {
 		return c.Usage()
 	}
-	if (len(args) == 1) && (args[0] == "all") {
-		args = g.AllModulesInOrder
-	} else {
-		for _, moduleName := range args {
-			err := g.ModuleExists(moduleName)
-			if err != nil {
-				fmt.Println(err)
-				fmt.Println("** status failed **")
-				return nil //g.Command_EX_ERR
-			}
+	for _, moduleName := range args {
+		if !g.HasModule(moduleName) {
+			return fmt.Errorf("%s doesn't exist\n", moduleName)
+		}
+
+		if g.IsRunning(moduleName) {
+			fmt.Print("[", g.ModuleApps[moduleName], "] ", g.Pid(moduleName), "\n")
+		} else {
+			fmt.Print("[", g.ModuleApps[moduleName], "] down\n")
 		}
 	}
-	for _, moduleName := range args {
-		g.CheckModuleStatus(moduleName)
-	}
-	return nil //g.Command_EX_OK
+	return nil
 }
