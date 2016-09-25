3a4500fa63451b8c7d647d2de6ebeb77b63eda00
chyeh <chyeh@cepave.com>
2016-09-25 14:22:58 +0800

    Fix the flags of subcommands

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 
 	"github.com/open-falcon/falcon-plus/cmd"
 	"github.com/spf13/cobra"
-	flag "github.com/spf13/pflag"
 )
 
 var versionFlag bool
 
 var RootCmd = &cobra.Command{
 	Use: "open-falcon",
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			fmt.Printf("Open-Falcon version %s, build %s\n", Version, GitCommit)
+			os.Exit(0)
+		}
+	},
 }
 
 func init() {
@@ -22,17 +27,13 @@ func init() {
 	RootCmd.AddCommand(cmd.Check)
 	RootCmd.AddCommand(cmd.Monitor)
 	RootCmd.AddCommand(cmd.Reload)
+
+	RootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "show version")
 	cmd.Start.Flags().BoolVar(&cmd.PreqOrderFlag, "preq-order", false, "start modules in the order of prerequisites")
 	cmd.Start.Flags().BoolVar(&cmd.ConsoleOutputFlag, "console-output", false, "print the module's output to the console")
-	flag.BoolVarP(&versionFlag, "version", "v", false, "show version")
-	flag.Parse()
 }
 
 func main() {
-	if versionFlag {
-		fmt.Printf("Open-Falcon version %s, build %s\n", Version, GitCommit)
-		os.Exit(0)
-	}
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
