202fc6145d78da2f75d4f01ba34d6e655d3ef93e
Zhang Yu <feiyuw@gmail.com>
2017-05-09 10:27:50 +0800

    net.port.listen support udp

--- modules/agent/funcs/portstat.go
+++ modules/agent/funcs/portstat.go
@@ -17,7 +17,13 @@ func PortMetrics() (L []*model.MetricValue) {
 		return
 	}
 
-	allListeningPorts, err := nux.ListeningPorts()
+	allTcpListeningPorts, err := nux.TcpPorts()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	allUdpListeningPorts, err := nux.UdpPorts()
 	if err != nil {
 		log.Println(err)
 		return
@@ -25,7 +31,9 @@ func PortMetrics() (L []*model.MetricValue) {
 
 	for i := 0; i < sz; i++ {
 		tags := fmt.Sprintf("port=%d", reportPorts[i])
-		if slice.ContainsInt64(allListeningPorts, reportPorts[i]) {
+		if slice.ContainsInt64(allTcpListeningPorts, reportPorts[i]) {
+			L = append(L, GaugeValue(g.NET_PORT_LISTEN, 1, tags))
+		} else if slice.ContainsInt64(allUdpListeningPorts, reportPorts[i]) {
 			L = append(L, GaugeValue(g.NET_PORT_LISTEN, 1, tags))
 		} else {
 			L = append(L, GaugeValue(g.NET_PORT_LISTEN, 0, tags))
