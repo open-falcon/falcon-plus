0b7b55d55625d3fb0da7e2921952286608a74255
Jack2chen <jack2chen@gmail.com>
2017-05-17 13:49:31 +0800

    rename chat to im

---
+++ modules/alarm/cron/im_sender.go
@@ -0,0 +1,45 @@
+package cron
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/open-falcon/falcon-plus/modules/alarm/g"
+	"github.com/open-falcon/falcon-plus/modules/alarm/model"
+	"github.com/open-falcon/falcon-plus/modules/alarm/redi"
+	"github.com/toolkits/net/httplib"
+	"time"
+)
+
+func ConsumeIM() {
+	for {
+		L := redi.PopAllIM()
+		if len(L) == 0 {
+			time.Sleep(time.Millisecond * 200)
+			continue
+		}
+		SendIMList(L)
+	}
+}
+
+func SendIMList(L []*model.IM) {
+	for _, im := range L {
+		IMWorkerChan <- 1
+		go SendIM(im)
+	}
+}
+
+func SendIM(im *model.IM) {
+	defer func() {
+		<-IMWorkerChan
+	}()
+
+	url := g.Config().Api.IM
+	r := httplib.Post(url).SetTimeout(5*time.Second, 30*time.Second)
+	r.Param("tos", im.Tos)
+	r.Param("content", im.Content)
+	resp, err := r.String()
+	if err != nil {
+		log.Errorf("send im fail, tos:%s, cotent:%s, error:%v", im.Tos, im.Content, err)
+	}
+
+	log.Debugf("send im:%v, resp:%v, url:%s", im, resp, url)
+}
