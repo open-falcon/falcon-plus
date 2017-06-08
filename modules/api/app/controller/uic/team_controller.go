4e3f040e86c984b5cd3900f76f3c450baca84023
zjh <junhui.zhang@newtouch.cn>
2017-06-08 11:06:35 +0800

    Support changing name when update team
    Signed-off-by: zjh <junhui.zhang@newtouch.cn>

--- modules/api/app/controller/uic/team_controller.go
+++ modules/api/app/controller/uic/team_controller.go
@@ -134,6 +134,7 @@ func CreateTeam(c *gin.Context) {
 type APIUpdateTeamInput struct {
 	ID      int    `json:"team_id" binding:"required"`
 	Resume  string `json:"resume"`
+	Name    string `json:"name"`
 	UserIDs []int  `json:"users"`
 }
 
@@ -169,7 +170,7 @@ func UpdateTeam(c *gin.Context) {
 		return
 	}
 
-	tm := uic.Team{Resume: cteam.Resume}
+	tm := uic.Team{Name:cteam.Name, Resume: cteam.Resume}
 	dt = db.Uic.Table("team").Where("id=?", cteam.ID).Update(&tm)
 	if dt.Error != nil {
 		h.JSONR(c, badstatus, dt.Error)
