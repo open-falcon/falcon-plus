9caae800509cee6f7cc82b1af3fd58f6ccd862df
yul <yul@inke.cn>
2018-01-27 15:00:43 +0800

    run gofmt
    Signed-off-by: yul <yul@inke.cn>

--- modules/api/app/controller/dashboard_graph/dashboard_graph_controller.go
+++ modules/api/app/controller/dashboard_graph/dashboard_graph_controller.go
@@ -206,7 +206,7 @@ func DashboardGraphUpdate(c *gin.Context) {
 	if inputs.FalconTags != "" {
 		d.FalconTags = inputs.FalconTags
 	}
-       d.Method = inputs.Method
+	d.Method = inputs.Method
 
 	graph := m.DashboardGraph{}
 	dt := db.Dashboard.Table("dashboard_graph").Model(&graph).Where("id = ?", gid).Updates(d)
@@ -215,12 +215,11 @@ func DashboardGraphUpdate(c *gin.Context) {
 		return
 	}
 
-       dt = db.Dashboard.Table("dashboard_graph").Model(&graph).Where("id = ?", gid).Update("method", d.Method)
+	dt = db.Dashboard.Table("dashboard_graph").Model(&graph).Where("id = ?", gid).Update("method", d.Method)
 	if dt.Error != nil {
 		h.JSONR(c, badstatus, dt.Error)
 		return
 	}
-
 
 	h.JSONR(c, map[string]int{"id": gid})
 
