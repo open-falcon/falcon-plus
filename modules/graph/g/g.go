254588c859bc1f3cc8d6dac6bdbc52c52a7e9efe
mdh6789 <mdh6789@gmail.com>
2018-04-27 16:56:04 +0800

    fix graph syncDisk goroutine waiting time

--- modules/graph/g/g.go
+++ modules/graph/g/g.go
@@ -43,14 +43,14 @@ const (
 	GAUGE           = "GAUGE"
 	DERIVE          = "DERIVE"
 	COUNTER         = "COUNTER"
-	DEFAULT_STEP    = 60      //s
-	MIN_STEP        = 30      //s
-	CLEAN_CACHE     = 86400   //s the step that clean GraphItems/historyCache Cache
-	CACHE_DELAY     = 1800    //s
-	CACHE_TIME      = 1800000 //ms
-	FLUSH_DISK_STEP = 1000    //ms
-	FLUSH_MIN_COUNT = 6       //  flush counter to disk when its number of monitoring data greater than FLUSH_MIN_COUNT
-	FLUSH_MAX_WAIT  = 86400   //s flush counter to disk if it not be flushed within FLUSH_MAX_WAIT seconds
+	DEFAULT_STEP    = 60                         //s
+	MIN_STEP        = 30                         //s
+	CLEAN_CACHE     = 86400                      //s the step that clean GraphItems/historyCache Cache
+	CACHE_DELAY     = MIN_STEP * FLUSH_MIN_COUNT //s
+	CACHE_TIME      = 1800000                    //ms
+	FLUSH_DISK_STEP = 1000                       //ms
+	FLUSH_MIN_COUNT = 6                          //  flush counter to disk when its number of monitoring data greater than FLUSH_MIN_COUNT
+	FLUSH_MAX_WAIT  = 86400                      //s flush counter to disk if it not be flushed within FLUSH_MAX_WAIT seconds
 )
 
 const (
