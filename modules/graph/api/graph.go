c27fb6d922ca7b24d63a95660759622652f7b17d
Jack0005 <mayingjie@xiaomi.com>
2018-07-02 21:23:55 +0800

    查询COUNTER类型数据时，当内存中数据的时间戳不连续时，计算出断点之后的COUNTER数据为NaN，计算错误。

--- modules/graph/api/graph.go
+++ modules/graph/api/graph.go
@@ -213,9 +213,12 @@ func (this *Graph) Query(param cmodel.GraphQueryParam, resp *cmodel.GraphQueryRe
 		itemIdx := 0
 		if dsType == g.DERIVE || dsType == g.COUNTER {
 			for ts < itemEndTs {
-				if itemIdx < items_size-1 && ts == items[itemIdx].Timestamp &&
-					ts == items[itemIdx+1].Timestamp-int64(step) {
-					val = cmodel.JsonFloat(items[itemIdx+1].Value-items[itemIdx].Value) / cmodel.JsonFloat(step)
+				if itemIdx < items_size-1 && ts == items[itemIdx].Timestamp {
+					if ts == items[itemIdx+1].Timestamp-int64(step) {
+						val = cmodel.JsonFloat(items[itemIdx+1].Value-items[itemIdx].Value) / cmodel.JsonFloat(step)
+					} else {
+						val = cmodel.JsonFloat(math.NaN())
+					}
 					if val < 0 {
 						val = cmodel.JsonFloat(math.NaN())
 					}
