249a60ef84e4abbd16e6ef425bca273ce8a9e8e5
Zhufeng Li <techmotive@163.com>
2018-10-25 18:21:13 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+tar -xzvf /home/ld-sgdev/zhufeng_li/workspace/src/github.com/open-falcon/falcon-plus/*gz -C .
+find . -name cfg.json |xargs sed -i 's/root:@tcp(127.0.0.1:3306)/root:123456@tcp(127.0.0.1:3306)/g'
+sed -i   's#"max_conns": 100,#"max_conns": 1000,#' api/config/cfg.json
+sed -i   's#"max_idle": 100,#"max_idle": 1000,#' api/config/cfg.json
+find . -name '*.json' |xargs sed -i 's#"log_level": "debug"#"log_level": "warn"#g'
+find . -name '*.json' |xargs sed -i 's#"debug": true,#"debug": false,#g'
