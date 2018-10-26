e0355ccb7b458eaeff7a1fffd0ea2ffc73875c09
Zhufeng Li <zhufeng.li@shopee.com>
2018-10-26 11:08:14 +0800

    modify script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+cd /home/ld-sgdev/workspace/open-falcon
 tar -xzvf /home/ld-sgdev/zhufeng_li/workspace/src/github.com/open-falcon/falcon-plus/*gz -C .
 find . -name cfg.json |xargs sed -i 's/root:@tcp(127.0.0.1:3306)/root:123456@tcp(127.0.0.1:3306)/g'
 sed -i   's#"max_conns": 100,#"max_conns": 1000,#' api/config/cfg.json
