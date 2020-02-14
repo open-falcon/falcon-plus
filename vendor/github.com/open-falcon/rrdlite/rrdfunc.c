ae99a756218a8cbf7a009493ec939e31f1e57f0a
laiwei <laiwei.ustc@gmail.com>
2020-02-14 22:31:48 +0800

    update vendor rrdlite

--- vendor/github.com/open-falcon/rrdlite/rrdfunc.c
+++ vendor/github.com/open-falcon/rrdlite/rrdfunc.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 #include "rrd.h"
 
 const char *rrdCreate(const char *filename, unsigned long step, 
 		time_t start, int argc, const char **argv) {
 	int ret;
 	ret = rrd_create_r(filename, step, start, argc, argv);
+    if (ret != 0){
+        printf("rrd_create %s, errno: %d\n", filename, ret);
+    }
 	return rrd_strerror(ret);
 }
 
@@ -12,6 +17,9 @@ const char *rrdUpdate(const char *filename, const char *template,
 		int argc, const char **argv) {
 	int ret;
 	ret = rrd_update_r(filename, template, argc, argv);
+    if (ret != 0){
+        printf("rrd_update %s, errno: %d\n", filename, ret);
+    }
 	return rrd_strerror(ret);
 }
 
@@ -19,13 +27,21 @@ const char *rrdUpdate(const char *filename, const char *template,
 const char *rrdInfo(rrd_info_t **info, char *filename) {
 	int ret = 0;
 	*info = rrd_info_r(filename, &ret);
+    if (ret != 0){
+        printf("rrd_info %s, errno: %d\n", filename, ret);
+    }
 	return rrd_strerror(ret);
 }
 
 const char *rrdFetch(int *ret, char *filename, const char *cf, time_t *start, 
 		time_t *end, unsigned long *step, unsigned long *ds_cnt, 
 		char ***ds_namv, double **data) {
+	//setbuf(stdout, NULL);
+	//setbuf(stderr, NULL);
 	*ret = rrd_fetch_r(filename, cf, start, end, step, ds_cnt, ds_namv, data);
+    if (*ret != 0){
+        printf("rrdfetch %s, errno: %d\n", filename, *ret);
+    }
 	return rrd_strerror(*ret);
 }
 
