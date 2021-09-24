8272d1bea8b98bea6a2edee1b2eea96fd3c33feb
shaneing <z.shane.ing@gmail.com>
2021-09-24 10:57:57 +0800

    chore: add docker compose (#867)
    * chore: add docker compose
    
    * chore: correct the replace function in ctrl.sh
    
    * use golang1.15  and alpine 3.13 in base image
    
    Co-authored-by: Vic Lai <laiwei.ustc@gmail.com>

--- config/confgen.sh
+++ config/confgen.sh
@@ -16,6 +16,7 @@ confs=(
     '%%MYSQL%%=root:@tcp(127.0.0.1:3306)'
     '%%PLUS_API_DEFAULT_TOKEN%%=default-token-used-in-server-side'
     '%%PLUS_API_HTTP%%=0.0.0.0:8080'
+    '%%AGENT_HOSTNAME%%='
  )
 
 configurer() {
